b4d898839b4130b6454b4c51899a84eb14e8cf2e
Elena Maslovskaya <elena.maslovski@yandex.ru>
2022-03-21 19:44:50 +0100

    .env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const bearerToken = (header) => header.replace('Bearer ', '');
 
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
   }
